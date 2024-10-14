5f1cc21e4752f30a9b1b08d7c7bf79b34bcf4582
Lorentz F. Barstad <29151133+lorentzfb@users.noreply.github.com>
2024-10-14 20:27:01 +0200

    sofle initial keymap

---
+++ keyboards/sofle/keymaps/lorentzfb_alpha/config.h
@@ -0,0 +1,16 @@
+// Copyright 2024 Santosh Kumar (@santosh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define TRI_LAYER_LOWER_LAYER 2
+#define TRI_LAYER_UPPER_LAYER 3
+#define TRI_LAYER_ADJUST_LAYER 5
+
+#define SPLIT_USB_DETECT
+
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
--- keyboards/sofle/sofle.c
+++ keyboards/sofle/sofle.c
@@ -119,9 +119,9 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
     }
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
             tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
         }
     } else if (index == 1) {
         if (clockwise) {
